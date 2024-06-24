b36f03f92d341a49d2c8716d2d7d6238a2b2223c
Yulber Espinoza <yulbertime@gmail.com>
2024-06-24 01:39:20 -0500

    Subir última modificación

--- js/app.js
+++ js/app.js
@@ -78,5 +78,53 @@ if(pregunta5.toUpperCase()===respuestaBuena.toUpperCase()){
     conteoNeg +=1;
 }
 
-alert("Las respuestas buenas son: "+conteoPos + " "+ "y respuestas negativas son: "+conteoNeg);
 
+const numAdivinar = Number(Math.random().toFixed(2)) * 100;
+alert(numAdivinar)
+
+for (let i = 0; i <4; i++) {
+
+  const numero = Number(prompt("Adivina un numero aleatorio del 1 al 100 "))
+   if (numero ===  numAdivinar) {
+       alert("Adivinaste el numero");
+       conteoPos +=1;
+       break;
+   } else{
+       if(numero>numAdivinar){
+        alert("Numero muy grande");
+       }else{
+        alert("Numero muy pequeño");
+       }
+       alert("Hacer otro intento");
+   }
+    if(i===4){
+          alert("Ya no tienes oportunidades");
+          conteoNeg +=1;
+        }
+}
+
+const posRespuestas =["Perú","Argentina","Brasil","Uruguay"];
+
+for (let i = 0; i < 6; i++) {
+  let respuestaPais = prompt("Digite un país de América del Sur");
+
+   if (respuestaPais.toLocaleLowerCase === posRespuestas[i].toLocaleLowerCase) {
+      alert("Encontraste el pais");
+      conteoPos +=1;
+      break;
+   }else{
+     alert("Sigue intentando");
+   }
+   
+   if(i===5){
+    alert("Se te acabaron los intentos");
+    conteoNeg +=1;
+alert("Posibles respuestas");
+for (let a = 0;a < 4; a++) {
+  alert(posRespuestas[a]);
+}
+
+   }
+}
+
+alert("Las respuestas buenas son: "+conteoPos + " "+ "y respuestas negativas son: "+conteoNeg);
